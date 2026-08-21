834ae6d551ba2b630f7cd6f9a738ebd29ee2b54a
nashysaimarkets <hello@nashaimarkets.com>
2026-08-21 22:57:10 +0100

    fix(pocket): load lossless dark-chart rendering

--- app/pocket/page.tsx
+++ app/pocket/page.tsx
@@ -10,6 +10,7 @@ import "./pocket-launch-v4.css";
 import "./pocket-final-tuning.css";
 import "./pocket-launch-v5.css";
 import "./pocket-launch-v6.css";
+import "./pocket-launch-v7.css";
 
 export const metadata: Metadata = {
   title: "Pocket Bullseye",
