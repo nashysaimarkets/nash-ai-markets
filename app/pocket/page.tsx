e2bfb306a677375a4985410fa2e3504704aba6fe
nashysaimarkets <hello@nashaimarkets.com>
2026-08-21 23:53:41 +0100

    style(pocket): activate follow-up and fingerprint tranche

--- app/pocket/page.tsx
+++ app/pocket/page.tsx
@@ -17,6 +17,7 @@ import "./pocket-launch-v10.css";
 import "./pocket-launch-v11.css";
 import "./pocket-launch-v12.css";
 import "./pocket-launch-v13.css";
+import "./pocket-launch-v14.css";
 
 export const metadata: Metadata = {
   title: "Pocket Bullseye",
