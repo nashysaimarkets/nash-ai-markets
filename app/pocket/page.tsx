df8700f15e606c5d72aa8c058a5410950b52b3c9
nashysaimarkets <hello@nashaimarkets.com>
2026-08-21 22:40:58 +0100

    feat(pocket): load v5 experience polish

--- app/pocket/page.tsx
+++ app/pocket/page.tsx
@@ -8,6 +8,7 @@ import "./pocket-launch-v2.css";
 import "./pocket-launch-v3.css";
 import "./pocket-launch-v4.css";
 import "./pocket-final-tuning.css";
+import "./pocket-launch-v5.css";
 
 export const metadata: Metadata = {
   title: "Pocket Bullseye",
