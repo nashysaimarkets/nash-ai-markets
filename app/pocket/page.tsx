4558c9a32ab77b3672d6e72556d9063ab618642f
nashysaimarkets <hello@nashaimarkets.com>
2026-08-28 09:53:09 +0100

    Pocket v1.1: update app/pocket/page.tsx

--- app/pocket/page.tsx
+++ app/pocket/page.tsx
@@ -33,6 +33,7 @@ import "./pocket-lock-on.css";
 import "./pocket-command-arena.css";
 import "./pocket-bubble-lab.css";
 import "./pocket-pattern-watch.css";
+import "./pocket-trust-retention.css";
 
 export const metadata: Metadata = {
   title: "Pocket Bullseye",
